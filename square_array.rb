6867e332f106b1aee3a1dda033fdb4a5c8d9cd15
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 00:03:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,8 @@ def square_array(array)
   # your code here
   
   new_array=[]
-  array.each do |num| sqrnum = (num ** 2) 
+  array.each do |num| 
+  sqrnum = (num ** 2) 
   new_array.push(sqrnum)
    return new_array
   end
