18290a514738edb85dcf011aeaf27edbcf4328b6
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 00:22:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -17,9 +17,10 @@ def square_array(array)
   
   new_array=[]
   
-  array.each do |num| new_array.push(num ** 2) end
+  array.each do |num| new_array.push(num ** 2) 
   
   
   new_array
+end
   end
  
